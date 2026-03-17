63c423702b7434313c7b59fa990cfbec604e4376
Your Name <your_email@example.com>
2026-03-17 08:13:47 +0200

    fix firmware tracking

---
+++ src/rover_firmware/firmware/simple_motor_control/simple_motor_control.ino
@@ -0,0 +1,26 @@
+#define L298N_enA 9
+#define L298N_in1 12
+#define L298N_in2 13
+double cmd = 0.0;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(L298N_enA, OUTPUT);
+  pinMode(L298N_in1, OUTPUT);
+  pinMode(L298N_in2, OUTPUT);
+  digitalWrite(L298N_in2,LOW);
+  digitalWrite(L298N_in1,HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()){
+    cmd = Serial.readString().toDouble();
+  }
+  analogWrite(L298N_enA , cmd * 100);
+
+}
